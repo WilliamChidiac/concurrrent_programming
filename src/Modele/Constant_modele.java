675e52280b38f1d7d3a1df496574e38a93d6b7a3
WilliamChidiacFunctori <william.chidiac@functori.com>
2024-03-10 15:51:50 +0100

    [Constant] added Constants file for View and Modele.

---
+++ src/Modele/Constant_modele.java
@@ -0,0 +1,9 @@
+package Modele;
+
+/**
+ * Constantes du modele
+ */
+public class Constant_modele{
+    public static final int VITESSE_DEPLACEMENT_UNITE = 5;
+    public static final int THREAD_DELAY_UNITE = 100;
+}
---
+++ src/View/Constant_view.java
@@ -0,0 +1,12 @@
+package View;
+
+/**
+ * Constantes de la vue
+ */
+public class Constant_view {
+    public static final int WIDTH_WINDOW = 800;
+    public static final int HEIGHT_WINDOW = 600;
+
+    public static final int REFRESH_RATE = 50;
+    public static final int RADIUS_UNIT = 10;
+}
