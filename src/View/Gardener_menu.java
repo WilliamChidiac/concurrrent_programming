f0020f4789b040a5340dd8f8558a7295f7fce033
Sushi <SushiGatomique@gmail.com>
2024-04-02 17:05:57 +0200

    [View] Refactor TopLayer and gardener_menu

--- src/View/Gardener_menu.java
+++ src/View/Gardener_menu.java
@@ -1,6 +1,5 @@
 package View;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.Point;
@@ -12,7 +11,7 @@ import javax.swing.JPanel;
 
 import Modele.Score_and_prices;
 
-public class Gardener_menu extends TopLayer {
+public class Gardener_menu extends JPanel {
 
     //Attribute that contains the map of the game
     private Main_panel map;
@@ -20,51 +19,41 @@ public class Gardener_menu extends TopLayer {
     /**
      * Constructor that creates the gardener's menu with the buttons interactions
      * @param main the map of the game
-     * @return a gardener's menu
      */
     public Gardener_menu(Main_panel main, Score_and_prices sp){
-        super("Gardener's actions :");
+        super(new GridLayout(2,1));
         this.map = main;
         //Panel that contains the buttonsPanel for the gardener
-        JPanel buttonsPanel = new JPanel(new GridLayout(10,2));
-        buttonsPanel.setBackground(Color.lightGray);
+        this.setBackground(Color.lightGray);
         //buttonsPanel.setPreferredSize(new Dimension(WIDTH/4, HEIGHT/16));
-        buttonsPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
+        this.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
 
         //Definitions and Add buttons to the buttonsPanel
-        JButton b1 = new JButton("Market");
-        b1.addActionListener(e -> System.out.println("go to the market"));
-        JButton b2 = new JButton("Plant");
-        b2.addActionListener(e ->
+        JButton b1 = new JButton("Plant");
+        b1.addActionListener(e ->
         {
             Point coordinate = Unite_controle_view.get_selected_unit().get_unite().get_current_location();
             map.add(new Plant_view(2, 3, 4,
             coordinate, sp ));
         }
         );
-        JButton b3 = new JButton("Take");
-        b3.addActionListener(e -> System.out.println("I take a plant"));
-        JButton b4 = new JButton("Stay");
-        b4.addActionListener(e -> map.stay());
-        //buttonsPanel.add(b1);
-        buttonsPanel.add(b2);
-        buttonsPanel.add(b3);
-        buttonsPanel.add(b4);
-        this.add(buttonsPanel, BorderLayout.SOUTH);
+        JButton b2 = new JButton("Stay");
+        b2.addActionListener(e -> map.stay());
+        this.add(b1);
+        this.add(b2);
     }
 
     /**
      * Main method to test the Gardener_menu class
-     * @param args
      */
     public static void main(String[] args) {
         //Test the Gardener_menu class
         JFrame frame = new JFrame();
         Score_and_prices sp = new Score_and_prices();
+
         frame.add(new Gardener_menu(null, sp));
         frame.pack();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800, 600);
         frame.setVisible(true);
     }
 }
--- src/View/TopLayer.java
+++ src/View/TopLayer.java
@@ -3,44 +3,62 @@ package View;
 import javax.swing.*;
 import java.awt.*;
 public class TopLayer extends JPanel{
+
+    //Attribute that contains the JPanel with a gridLayout with the textual informations
+    private JPanel textPanel;
     /**
      * Constructor that creates the top layer with the title
      * @param str the title of the top layer
-     * @return a top layer
      */
     public TopLayer(String str){
-        this.setLayout( new BorderLayout());
+        super(new BorderLayout());
         this.setBackground(Color.darkGray);
+        this.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
+        this.setPreferredSize(new Dimension(Constant_view.WIDTH_WINDOW/4, Constant_view.HEIGHT_WINDOW));
+
+        this.textPanel = new JPanel(new GridLayout(2,1));
+        JTextField score = new JTextField("Score : 0");
+        score.setEditable(false);
+        score.setHorizontalAlignment(JTextField.CENTER);
+        score.setBackground(Color.LIGHT_GRAY);
+        this.textPanel.add(score);
 
         JTextField top = new JTextField(str);
         top.setEditable(false);
         top.setHorizontalAlignment(JTextField.CENTER);
         top.setBackground(Color.LIGHT_GRAY);
-        top.setPreferredSize(new Dimension(WIDTH/4, HEIGHT/32));
-        this.add(top, BorderLayout.CENTER);
+        this.textPanel.add(top);
+
+        this.add(this.textPanel, BorderLayout.NORTH);
+    }
+
+    //Method setTitle that changes the text of the second JTextField with the new title
+    public void setTitle(String str){
+        ((JTextField) this.textPanel.getComponent(1)).setText(str);
     }
 
     /**
      * Main method to test the TopLayer class
-     * @param args
      */
     public static void main(String[] args) {
         //Test the TopLayer class
         JFrame frame = new JFrame();
-        JPanel top = new TopLayer("Gardener's "+"1"+" actions :");
+        TopLayer top = new TopLayer("Gardener's "+"1"+" actions :");
 
         //Panel buttons
         JPanel buttonsPanel = new JPanel(new GridLayout(10,2));
-        buttonsPanel.add(new JButton("Market"));
+        JButton b1 = new JButton("Change title");
+        b1.addActionListener(e -> top.setTitle("Menu"));
+        buttonsPanel.add(b1);
+
         buttonsPanel.add(new JButton("Market2"));
         buttonsPanel.add(new JButton("Market3"));
         buttonsPanel.add(new JButton("Market4"));
 
-        top.add(buttonsPanel, BorderLayout.SOUTH);
+        top.add(buttonsPanel, BorderLayout.CENTER);
         frame.add(top);
-        frame.pack();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800, 600);
+        frame.pack();
         frame.setVisible(true);
     }
 }
