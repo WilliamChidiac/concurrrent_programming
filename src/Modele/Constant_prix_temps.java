2935c53b7e37360c9394f87001a1b61d1f6fe290
eliott <eliott.rigobert@u-psud.fr>
2024-04-16 23:09:15 +0200

    16/04/24 : Cst, ISV

---
+++ src/Modele/Constant_prix_temps.java
@@ -0,0 +1,13 @@
+package Modele;
+
+public class Constant_prix_temps {
+
+    public static final int roseP = 15;
+    public static final int roseT = 5;
+    public static final int tulP = 10;
+    public static final int tulT = 2;
+    public static final int lysP = 9;
+    public static final int lysT = 3;
+    public static final int magnoliaP = 17;
+    public static final int magnoliaT = 7;
+}
---
+++ src/View/InfoShopView.java
@@ -0,0 +1,38 @@
+package View;
+
+import javax.swing.*;
+import Modele.*;
+import java.awt.*;
+
+public class InfoShopView extends JFrame{
+
+    public InfoShopView(EspecePlante plante) {
+        setTitle("Informations sur " + plante.getNom());
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        // Ajouter des composants pour afficher les informations sur la plante
+        JLabel nameLabel = new JLabel("Nom : " + plante.getNom());
+        JLabel descriptionLabel = new JLabel("Description : " + plante.getDescription());
+        JLabel conseil1Label = new JLabel("Conseil 1 : " + plante.getConseil1());
+        JLabel conseil2Label = new JLabel("Conseil 2 : " + plante.getConseil2());
+        JLabel conseil3Label = new JLabel("Conseil 3 : " + plante.getConseil3());
+        JLabel prixLabel = new JLabel("Prix : " + plante.getPrix());
+        JLabel timeGrowthLabel = new JLabel("Temps de croissance : " + plante.getTimeGrowth() + " jours");
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(7, 1));
+        panel.add(nameLabel);
+        panel.add(descriptionLabel);
+        panel.add(conseil1Label);
+        panel.add(conseil2Label);
+        panel.add(conseil3Label);
+        panel.add(prixLabel);
+        panel.add(timeGrowthLabel);
+
+        add(panel);
+
+        pack();
+        setLocationRelativeTo(null); // Centre la fenêtre sur l'écran
+        setVisible(true);
+    }
+}
